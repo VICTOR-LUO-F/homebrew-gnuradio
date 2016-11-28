bca9014db8ef978640ab8702e552b127eaa21a01
Mike Causer <mcauser@gmail.com>
2016-11-29 00:30:30 +1100

    Libfec: use sha256

--- libfec.rb
+++ libfec.rb
@@ -3,13 +3,13 @@ require "formula"
 class Libfec < Formula
   homepage "http://www.ka9q.net/code/fec/"
   url "http://www.ka9q.net/code/fec/fec-3.0.1.tar.bz2"
-  sha1 "d5cac0680b055c7808e3b33876ab4e819cd318ae"
+  sha256 "4201f6c80fe3fb283806bf41a74ea3476d783081e7cd6d09f12406894e6f567c"
 
   depends_on 'gcc'
 
   patch :p1 do
     url "http://lodge.glasgownet.com/bitsnbobs/kg_fec-3.0.1.patch"
-    sha1 "fc25babccb5ce3e783619f886c858b28f42652b9"
+    sha256 "8b63e6db3d2d1f0e3861181dc6eb41e8076147e938bd226ee6a238c4b4a4bed7"
   end
 
   def install
